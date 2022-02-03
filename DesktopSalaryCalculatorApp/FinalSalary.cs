1c4ab83e80f0cfeb528415e2763184a5888b2438
Your Name <you@example.com>
2022-02-04 00:17:09 +0600

    Add project files.

---
+++ DesktopSalaryCalculatorApp/FinalSalary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopSalaryCalculatorApp
+{
+    class FinalSalary
+    {
+        public string employeeName;
+        public int basicAmount;
+        public int homeRent;
+        public int medicalAllowance;
+
+        public string GetFullSalary()
+        {
+            
+            int salary = basicAmount + (basicAmount / 100 * homeRent) + (basicAmount / 100 * medicalAllowance);
+            return salary.ToString();
+        }
+    }
+}
---
+++ DesktopSalaryCalculatorApp/SalaryCalculatorUi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopSalaryCalculatorApp
+{
+    public partial class SalaryCalculatorUi : Form
+    {
+        public SalaryCalculatorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowMeSalaryButton_Click(object sender, EventArgs e)
+        {
+            FinalSalary finalSalary = new FinalSalary();
+            finalSalary.employeeName = employeeNameTextBox.Text;
+            finalSalary.basicAmount = Convert.ToInt32(basicAmountTextBox.Text);
+            finalSalary.homeRent = Convert.ToInt32(homeRentTextBox.Text);
+            finalSalary.medicalAllowance = Convert.ToInt32(medicalAllowanceTextBox.Text);
+
+            string Salary = finalSalary.GetFullSalary();
+            string Name = finalSalary.employeeName;
+            MessageBox.Show(Name + Salary);
+        }
+    }
+}
